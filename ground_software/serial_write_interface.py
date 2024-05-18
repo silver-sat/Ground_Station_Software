d1428e7f227f0e9974328d736b8927d6710e6404
bscohen06 <bscohen06@gmail.com>
2024-05-18 16:22:33 -0400

    radio interface

---
+++ ground_software/serial_write_interface.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""
+ @file serial_write_interface.py
+ @author Lee A. Congdon (lee@silversat.org)
+ @author Benjamin S. Cohen (ben@silversat.org)
+ @author Dominik Honzak (dominik@silversat.org)
+ @brief SilverSat User and radio Doppler interface
+ @version 1.0.0
+ @date 2024-05-18
+ 
+ This program provides the radio interface for sending commands
+ 
+"""
+
+# imports
+import sqlite3
+import serial
+import time
+
+BAUD_RATE = 57600
+
+
+def serial_write():
+
+    # open database
+    connection = sqlite3.connect("instance/radio.db")
+    cursor = connection.cursor()
+
+    # naming serial connection
+    serial_port = "/dev/ttys010"
+
+    while True:
+        try:
+            # opening serial connection
+            radio_serial = serial.Serial(serial_port, BAUD_RATE)
+            break
+        except:
+            time.sleep(5)
+            continue
+
+    while True:
+        try:
+            result = cursor.execute(
+                "SELECT * FROM transmissions WHERE status='pending' ORDER BY id ASC LIMIT 1"
+            )
+            id, timestamp, command, status = result.fetchone()
+            radio_serial.write(command)
+            cursor.execute(
+                "UPDATE transmissions SET status = 'transmitted' WHERE id = ?", (id,)
+            )
+            connection.commit()
+        except:
+            time.sleep(1)
+
+
+if __name__ == "__main__":
+    serial.write()
